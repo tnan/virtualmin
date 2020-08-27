8f55e6ffd51d1ace8572205a6e2abe50f27d9a8d
tnan <mr.tnan@live.com>
2020-08-27 09:36:14 -0700

    Update virtualmin.sh

--- virtualmin.sh
+++ virtualmin.sh
@@ -1,1 +1,8 @@
-wget http://software.virtualmin.com/gpl/scripts/install.sh && printf 'y\nvirtualmin.the-entertainment.com\n' | /bin/sh install.sh && reboot
+cd /root
+apt update
+apt upgrade -y
+yum makecache
+yum update -y
+wget http://software.virtualmin.com/gpl/scripts/install.sh
+printf 'y\nvirtualmin.the-entertainment.com\n' | /bin/sh install.sh
+reboot
