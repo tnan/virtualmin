936a85a3875213e4086cad97deafb4a73c0dbe9d
tnan <mr.tnan@live.com>
2020-08-26 23:59:46 -0700

    Update and rename virtualmin_tnan.sh to virtualmin.sh

---
+++ virtualmin.sh
@@ -0,0 +1,1 @@
+wget http://software.virtualmin.com/gpl/scripts/install.sh && printf 'y\nvirtualmin.the-entertainment.com\n' | sudo /bin/sh install.sh && reboot
--- virtualmin_tnan.sh
+++
@@ -1,1 +0,0 @@
-screen -L -dm bash -c "wget http://software.virtualmin.com/gpl/scripts/install.sh && printf 'y\nvirtualmin.the-entertainment.com\n' | sudo /bin/sh install.sh && reboot"
