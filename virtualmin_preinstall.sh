1ebd42213245f366bfca2e61004249c2bcfb216c
tnan <mr.tnan@live.com>
2020-08-27 14:13:05 -0700

    Update and rename virtualmin.sh to virtualmin_preinstall.sh

--- virtualmin_preinstall.sh
+++ virtualmin_preinstall.sh
@@ -1,10 +1,8 @@
-cd /root
+cd ~
 apt update
 apt upgrade -y
 yum makecache
 yum update -y
-yum install screen -y
-apt install screen -y
 wget http://software.virtualmin.com/gpl/scripts/install.sh
 printf 'y\nvirtualmin.the-entertainment.com\n' | /bin/sh install.sh
 reboot
