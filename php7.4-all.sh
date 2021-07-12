72681e85f1cb94b0a2eaa5fd7213280ff7a44b43
tnan <mr.tnan@live.com>
2021-07-12 01:49:43 -0700

    Delete php7.4-all.sh

--- php7.4-all.sh
+++
@@ -1,6 +0,0 @@
-add-apt-repository ppa:ondrej/php -y
-apt update
-apt upgrade -y
-apt autoremove -y
-wget https://raw.githubusercontent.com/tnan/virtualmin/master/php/preferences -O /etc/apt/preferences
-apt install php7.4* -y
