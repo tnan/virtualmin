a7c08ba4ec2c07fbdd74a39fb8f944c040ebb358
tnan <mr.tnan@live.com>
2021-07-13 00:06:18 -0700

    Create virtualmin-install-curl7.70.0.sh

---
+++ curl/virtualmin-install-curl7.70.0.sh
@@ -0,0 +1,17 @@
+cd /root/
+apt update
+apt install -y libssl-dev autoconf libtool make cmake git build-essential libcurl4 openssl libssl-dev libssh-dev zlib1g-dev zlib1g zlib1g-dev libbrotli-dev brotli libkrb5-dev libldap2-dev librtmp-dev libpsl-dev libnghttp2-dev
+cd /usr/local/src
+rm -rf curl*
+wget https://curl.haxx.se/download/curl-7.70.0.zip
+unzip curl-7.70.0.zip
+cd curl-7.70.0
+./buildconf
+./configure --with-ssl
+make
+make install
+rm -rf curl*
+cd /root/
+cp /usr/local/bin/curl /usr/bin/curl
+ldconfig
+curl -V
