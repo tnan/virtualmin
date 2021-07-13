c1cfae95fe4ca4103b61d6fa6102e9bee7fd8df2
tnan <mr.tnan@live.com>
2021-07-13 00:05:26 -0700

    Create virtualmin-install-php7.4-all.sh

---
+++ php/virtualmin-install-php7.4-all.sh
@@ -0,0 +1,7 @@
+cd /root/
+add-apt-repository ppa:ondrej/php -y
+apt update
+apt upgrade -y
+apt autoremove -y
+wget https://raw.githubusercontent.com/tnan/virtualmin/master/php/preferences -O /etc/apt/preferences
+apt install php7.4* -y
