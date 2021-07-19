a98953b4950affe3cc5775eaf322922abdc96bff
tnan <mr.tnan@live.com>
2021-07-18 17:55:17 -0700

    Update virtualmin-mysql8-php7.4full-php8.0full.sh

--- virtualmin-mysql8-php7.4full-php8.0full.sh
+++ virtualmin-mysql8-php7.4full-php8.0full.sh
@@ -29,6 +29,11 @@ wget https://raw.githubusercontent.com/tnan/virtualmin/master/php/preferences -O
 apt install php7.4* php8.0* -y
 #PHP Install
 
+#Systemd Config
+sed -i 's/#DefaultTimeoutStartSec=90s/DefaultTimeoutStartSec=900s/g' /etc/systemd/system.conf
+sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=900s/g' /etc/systemd/system.conf
+#Systemd Config
+
 #Time Execution
 exe_time="$(($SECONDS / 3600)) hrs $((($SECONDS / 60) % 60)) min $(($SECONDS % 60)) sec"
 echo "Done! | Execution Date: $(date) | Execution Time: $exe_time"
