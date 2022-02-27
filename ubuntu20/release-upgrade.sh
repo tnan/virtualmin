b385d13302112e2e0d4884af421c4ec5b98dd61b
tnan <mr.tnan@live.com>
2022-02-27 15:45:56 -0800

    Update release-upgrade.sh

--- ubuntu20/release-upgrade.sh
+++ ubuntu20/release-upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/
 
-DEBIAN_FRONTEND=noninteractive do-release-upgrade -f DistUpgradeViewNonInteractive 2>&1|tee /root/release-upgrade.log
+DEBIAN_FRONTEND=noninteractive do-release-upgrade -f DistUpgradeViewNonInteractive
 
 wget https://github.com/tnan/virtualmin/raw/master/ubuntu20/rc.local_virtualmin -O /etc/rc.local
 chmod +x /etc/rc.local
