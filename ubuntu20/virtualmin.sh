933bec67653efef1200b4565a5ec58d655239df4
tnan <mr.tnan@live.com>
2022-02-27 23:32:07 -0800

    Update virtualmin.sh

--- ubuntu20/virtualmin.sh
+++ ubuntu20/virtualmin.sh
@@ -15,7 +15,7 @@ rm /etc/apt/preferences
 
 #PHP Ioncube
 cd /root/
-wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.zip -O ioncube_loaders_lin_x86-64.zip
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.zip -O /root/ioncube_loaders_lin_x86-64.zip
 unzip ioncube_loaders_lin_x86-64.zip
 wget https://raw.githubusercontent.com/tnan/virtualmin/master/ioncube/ioncube.ini -O /etc/php/7.4/mods-available/ioncube.ini
 cp /root/ioncube/ioncube_loader_lin_7.4.so /usr/lib/php/20190902/ioncube.so
@@ -25,6 +25,7 @@ ln -s /etc/php/7.4/mods-available/ioncube.ini /etc/php/7.4/cli/conf.d/00-ioncube
 ln -s /etc/php/7.4/mods-available/ioncube.ini /etc/php/7.4/fpm/conf.d/00-ioncube.ini
 ln -s /etc/php/7.4/mods-available/ioncube.ini /etc/php/7.4/phpdbg/conf.d/00-ioncube.ini
 rm -rf /root/ioncube/
+rm /root/ioncube_loaders_lin_x86-64.zip
 #PHP Ioncube
 
 #Systemd Config
