2bdfc0a870e4ab99f667c3e9efc815d81ccfdb8c
tnan <mr.tnan@live.com>
2022-02-27 20:43:24 -0800

    Update release-upgrade.sh

--- ubuntu20/release-upgrade.sh
+++ ubuntu20/release-upgrade.sh
@@ -3,7 +3,7 @@ cd /root/
 
 DEBIAN_FRONTEND=noninteractive do-release-upgrade -f DistUpgradeViewNonInteractive
 
-wget https://github.com/tnan/virtualmin/raw/master/ubuntu20/rc.local_virtualmin -O /etc/rc.local
+wget https://github.com/tnan/virtualmin/raw/master/ubuntu20/%5Cetc%5Crc.local_virtualmin -O /etc/rc.local
 chmod +x /etc/rc.local
 systemctl enable rc-local
 
