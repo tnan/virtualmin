db74ecddef82a12cb0d10dd5559702cddb7b7a41
tnan <mr.tnan@live.com>
2022-02-27 20:39:52 -0800

    Update upgrade.sh

--- ubuntu20/upgrade.sh
+++ ubuntu20/upgrade.sh
@@ -7,9 +7,6 @@ apt install -f -y
 apt autoremove -y
 apt autoclean -y
 
-wget https://github.com/tnan/virtualmin/raw/master/ubuntu20/release-upgrade.sh -O /root/release-upgrade.sh
-chmod +x /root/release-upgrade.sh
-
 wget https://github.com/tnan/virtualmin/raw/master/ubuntu20/%5Cetc%5Crc.local_release-upgrade -O /etc/rc.local
 chmod +x /etc/rc.local
 systemctl enable rc-local
