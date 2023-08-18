9229f186e1cec5574f0bd815aec192c7b4fc3182
tnan <mr.tnan@live.com>
2023-08-17 23:04:31 -0700

    Update virtualmin-install-php7.4-all.sh

--- php/virtualmin-install-php7.4-all.sh
+++ php/virtualmin-install-php7.4-all.sh
@@ -3,5 +3,5 @@ add-apt-repository ppa:ondrej/php -y
 apt update
 apt upgrade -y
 apt autoremove -y
-wget https://raw.githubusercontent.com/tnan/virtualmin/master/php/preferences -O /etc/apt/preferences
+wget https://github.com/tnan/virtualmin/raw/master/php/preferences-php7.4 -O /etc/apt/preferences
 apt install php7.4* -y
